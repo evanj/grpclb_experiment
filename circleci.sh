6c3510146b59a085e661ad61826b22455303e1bf
Evan Jones <ej@evanjones.ca>
2020-03-22 11:44:20 -0400

    Fix build/test/go.mod/circleci configuration

---
+++ circleci.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Runs checks on CircleCI
+
+set -euf -o pipefail
+
+# echo commands
+set -x
+
+# Run tests
+go test -mod=readonly -race ./...
+
+# go test only checks some vet warnings; check all
+go vet -mod=readonly ./...
+
+# cd /tmp to not change go.mod/go.sum for golint TODO: Use tools.go:
+# https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+(cd /tmp && go get golang.org/x/lint/golint)
+golint --set_exit_status ./...
+
+diff -u <(echo -n) <(gofmt -d .)
+
+# require that we use go mod tidy. TODO: there must be an easier way?
+go mod tidy
+CHANGED=$(git status --porcelain --untracked-files=no)
+if [ -n "${CHANGED}" ]; then
+    echo "ERROR files were changed:" > /dev/stderr
+    echo "$CHANGED" > /dev/stderr
+    exit 10
+fi
